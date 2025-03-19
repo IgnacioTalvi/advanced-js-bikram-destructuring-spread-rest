7bf970095d7817084a9816b96de88da8b2907b02
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-03-19 13:32:40 +0100

    Ejercicio_1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //RESUELVE LOS EJERCICIOS AQUÍ
+
+// Ejercicio 1
+const empleados = [
+  { name: "Luis", email: "Luis@gmail.com" },
+  { name: "Ana", email: "Ana@gmail.com" },
+  { name: "Andrea", email: "Andrea@gmail.com" },
+];
+
+let ana = empleados[1];
+console.log(ana);
+
+// Ejercicio 2
