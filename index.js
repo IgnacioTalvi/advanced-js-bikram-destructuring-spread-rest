729b2c91d27ba5b90b112ef6da7cf92e717f48a1
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-03-19 13:56:02 +0100

    Ejercicio_2_3_4_5

--- index.js
+++ index.js
@@ -11,3 +11,43 @@ let ana = empleados[1];
 console.log(ana);
 
 // Ejercicio 2
+
+let emailLuis = empleados[0].email;
+console.log(emailLuis);
+
+// Ejercicio 3
+
+let a = 5;
+let b = 3;
+
+[a, b] = [b, a];
+
+console.log(a, b);
+
+// Ejercicio 4
+
+const HIGH_TEMPERATURES = {
+  yesterday: 30,
+  today: 35,
+  tomorrow: 32,
+};
+
+const maximaHoy = HIGH_TEMPERATURES.today;
+const maximaManana = HIGH_TEMPERATURES.tomorrow;
+console.log(maximaHoy);
+console.log(maximaManana);
+
+// Ejercicio 5
+
+function sumEveryOther(...numeros) {
+  let total = 0;
+
+  for (let i = 0; i < numeros.length; i++) {
+    total += numeros[i];
+  }
+  return total;
+}
+
+console.log(sumEveryOther(1, 2, 3, 4, 5));
+
+// Ejercicio 6
