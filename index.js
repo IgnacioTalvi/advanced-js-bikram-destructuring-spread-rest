fe13b4d4a80a2e4e8e32ae323314dfe04390acfa
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-03-19 16:15:13 +0100

    Ejercicio_2_3_4_5_6_7_8

--- index.js
+++ index.js
@@ -51,3 +51,28 @@ function sumEveryOther(...numeros) {
 console.log(sumEveryOther(1, 2, 3, 4, 5));
 
 // Ejercicio 6
+
+function addOnlyNums(...numeros) {
+  let numeroActual = 0;
+  for (let i = 0; i < numeros.length; i++)
+    if (numeroActual !== "") {
+      numeroActual + numeros[i];
+      {
+        return numeroActual;
+      }
+    }
+
+  console.log(addOnlyNums(1, "perro", 6, "gato", "ratón", 7));
+}
+
+// Ejercicio 7
+
+function countTheArgs(...args) {
+  return args.length;
+}
+
+// let returnArgumentCount = (...args) => {
+//   return args.length;
+// };
+
+console.log(countTheArgs(1, "perro", 6, "gato", "ratón", 7));
